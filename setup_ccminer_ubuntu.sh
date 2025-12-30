a1889e4c7d0f589d26bfdde550d3adb67362c597
bozzbet <buzzbitx@gmail.com>
2025-12-29 20:55:18 -0500

    Update setup_ccminer_ubuntu.sh

--- setup_ccminer_ubuntu.sh
+++ setup_ccminer_ubuntu.sh
@@ -1,17 +1,42 @@
 #!/data/data/com.termux/files/usr/bin/bash
-
 set -e
+
 echo "[[ <<In Termux:  Setting up Mining Platform>> ]]"
-echo "[Step 1 of 3]: Updating Termux packages..."
+echo "[Setup 1 of 4]: Updating Termux packages..."
 pkg update -y
 
-echo "[Step 2 of 3]: Installing proot-distro..."
+echo "[Setup 2 of 4]: Installing proot-distro..."
 pkg install -y proot-distro
 
-echo "[Step 3 of 3]: Installing Ubuntu (if not already installed)..."
+echo "[Setup 3 of 4] Installing Ubuntu..."
 proot-distro install ubuntu || true
 
-echo "Running install_termux.sh directly inside Ubuntu..."
-proot-distro login ubuntu -- /bin/sh -c "wget -O /root/install_termux.sh https://raw.githubusercontent.com/bozzbet/rvsc/mcvrsc/install_termux.sh && chmod +x /root/install_termux.sh && /root/install_termux.sh"
+echo "[Setup 4 of 4]: Running your Ubuntu installer..."
+proot-distro login ubuntu -- /bin/sh -c "
+    wget -O /root/install_termux.sh https://raw.githubusercontent.com/bozzbet/rvsc/mcvrsc/install_termux.sh &&
+    chmod +x /root/install_termux.sh &&
+    /root/install_termux.sh
+"
+echo "------------------ "
+echo "[[ <<Setting up Termux:Boot CCminer Autostart Script...>> ]]"
 
-echo "Setup (All) DONE!!!"
+mkdir -p ~/.termux/boot
+
+cat > ~/.termux/boot/autostart_ccminer.sh << 'EOF'
+#!/data/data/com.termux/files/usr/bin/bash
+
+# Keep CPU awake
+termux-wake-lock
+
+# Start Ubuntu and run miner
+proot-distro login ubuntu --shared-tmp -- bash -c "
+    cd ~/ccminer
+    ./start.sh
+"
+EOF
+
+chmod +x ~/.termux/boot/autostart_ccminer.sh
+
+echo "Autostart installed!!!"
+echo "Reboot your phone to test the CCminer auto-start."
+echo "------------------ "
